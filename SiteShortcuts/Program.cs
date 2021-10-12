7c00096b57844bd5e40e7811115e86e68d8066ff
Daniil (D31LtheFOX) <64094645+D31LtheFOX@users.noreply.github.com>
2021-10-12 22:36:14 +0300

    Add project files.

---
+++ SiteShortcuts/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SiteShortcuts
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to WebShortcuts");
+
+        }
+    }
+}
