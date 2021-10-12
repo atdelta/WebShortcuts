2c629e1e054660f5361477c3ade7eaf3589d4399
Daniil (D31LtheFOX) <64094645+D31LtheFOX@users.noreply.github.com>
2021-10-13 00:11:52 +0300

    starting

--- SiteShortcuts/Program.cs
+++ SiteShortcuts/Program.cs
@@ -4,16 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
+using System.Diagnostics;
 
 namespace SiteShortcuts
 {
     class Program
     {
-        public static void Main()
+        public static void Main() //
         {
             Console.WriteLine("Welcome to WebShortcuts");
-
+            Thread.Sleep(5000);
+            Console.WriteLine("Console WebShortcut tool");
+            Thread.Sleep(5000);
+            Console.WriteLine("Do you wanna to see availible shortcuts?");
+            string doneedhelp = Console.ReadLine();
+            doneedhelp = doneedhelp.ToUpper();
+            doneedhelp = doneedhelp.Substring(0, 1);
+            if (doneedhelp == Y)
+            {
+                Console.WriteLine(others.help);
+            }
         }
     }
 }
---
+++ SiteShortcuts/others.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiteShortcuts
+{
+    internal class others
+    {
+        public static void help()
+        {
+            Console.WriteLine();
+            Console.WriteLine("List of availible shortcuts");
+            Console.WriteLine("google        |      github");
+            Console.WriteLine("spotify       |  newgrounds");
+            Console.WriteLine("soundcloud    |     youtube");
+            Console.WriteLine("discord       |       steam");
+            Console.WriteLine();
+        }
+
+        public static void changelang()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Change to russian or english? (write Ru or En");
+            string lang = Console.ReadLine();
+            if (lang == "Ru")
+            {
+                language = Program.Ru();
+            }
+            if (lang == "En")
+            {
+                language = Program.En();
+            }
+            else
+            {
+                changelang();
+            }
+        }
+    }
+}
